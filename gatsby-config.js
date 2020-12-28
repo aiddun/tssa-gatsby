1b5c7165bd8f044b8cf0cf1e7114b586dff1027e
Aidan Dunlap <10292904+aiddun@users.noreply.github.com>
2020-12-28 01:41:49 -0500

    fix purge config

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,12 +66,12 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+      resolve: `gatsby-plugin-purgecss`,
       options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+        develop: true,
+        tailwind: true, // Enable tailwindcss support
       },
-    }, // must be after other CSS plugins
+    },
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
